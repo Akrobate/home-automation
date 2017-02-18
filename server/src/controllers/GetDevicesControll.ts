c94f60f9e7fe2c54ba42f5cf6f72c8cd04c20be5
Artiom FEDOROV <fedorov.artiom@gmail.com>
2017-02-18 23:49:52 +0100

    Controller devicecontroll init, api fix

---
+++ server/src/controllers/GetDevicesControll.ts
@@ -0,0 +1,23 @@
+/**
+ *
+ */
+
+export class GetDevicesControll {
+
+    public constructor() {
+        // console.log('contructed Users2ReadId')
+    }
+
+    public process(params: any): Promise<any> {
+        return new Promise((resolve: any, reject: any)=>{
+            console.log(params)
+            let e = false
+            if (e) {
+                reject()
+            }
+            resolve(
+                {controller:'contructed Users2ReadId'}
+            )
+        })
+    }
+}

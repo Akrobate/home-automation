e7f1bc3e4e15bcb4b8724022163e2fc85aabac9b
Artiom FEDOROV <fedorov.artiom@gmail.com>
2017-02-19 23:37:31 +0100

    Adding configuration to the camera-server

--- camera-server/src/configurations/DevicesConfigurationLoader.ts
+++ camera-server/src/configurations/DevicesConfigurationLoader.ts
@@ -7,11 +7,4 @@ var fs = require('fs')
 const configuration_filename = 'devices.configuration.yaml'
 let used_configuration = yml_parser.safeLoad(fs.readFileSync(process.cwd() + '/' + configuration_filename, 'utf8'))
 
-let identities = used_configuration.identities
-console.log(identities)
-for(let item in used_configuration.devices) {
-    let identity_name = used_configuration.devices[item].identity;
-    used_configuration.devices[item].identity = identities[identity_name]
-}
-
 export let DevicesConfiguration: Object = used_configuration.devices

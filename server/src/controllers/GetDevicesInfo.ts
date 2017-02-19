e68691599c760487fbb788753401e3739e781ba6
Artiom FEDOROV <fedorov.artiom@gmail.com>
2017-02-19 05:03:45 +0100

    Method devices/info works

--- server/src/controllers/GetDevicesInfo.ts
+++ server/src/controllers/GetDevicesInfo.ts
@@ -4,57 +4,16 @@
 
 'use strict'
 
-const exec = require('child_process').exec
-const Joi = require('joi');
-const debug = true
+import {DevicesConfiguration} from '../configurations/DevicesConfigurationLoader'
+let config:any = DevicesConfiguration
 
-export class GetDevicesControll {
-
-    public constructor() {
-        // console.log('contructed Users2ReadId')
-    }
-
+export class GetDevicesInfo {
     public process(params: any): Promise<any> {
-
-        // console.log(params.query)
         return new Promise((resolve: any, reject: any)=>{
-
-            let p = params.query;
-
-            const schema = Joi.object().keys({
-                group: Joi.number().min(0).max(100).required(),
-                identity: Joi.number().min(0).max(67108863).required(),
-                id: Joi.number().min(0).max(100).required(),
-                command: Joi.boolean().required()
-            })
-
-            const result = Joi.validate(p, schema);
-            if (result.error !== null) {
-                // console.log(JSON.stringify(result, null, 2))
-                return reject(result.error)
-            }
-
-            let command = 'off'
-            if (p.command) {
-                command = 'on'
-            }
-            // let cmd = 'sudo ./send 0 52423867 1 on'
-            let cmd = 'sudo ./send ' + p.group + ' ' + p.identity + ' ' + p.id + ' ' + command
-
-            if (debug) {
-                console.log(cmd)
-                return resolve({controller: 'GetDevicesControll', message: cmd })
+            if (Object.keys(params.query).length === 0) {
+                return resolve(config)
             } else {
-                exec(cmd, function(error, stdout, stderr) {
-                    console.log("done...")
-                    console.log("error")
-                    console.log(error)
-                    console.log("stdout")
-                    console.log(stdout)
-                    console.log("stderr")
-                    console.log(stderr)
-                    return resolve({controller: 'GetDevicesControll'})
-                })
+                return reject({name: 'ValidationError', details: "No parameters required"})
             }
         })
     }
